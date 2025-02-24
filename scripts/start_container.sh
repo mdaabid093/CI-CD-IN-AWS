b74b30fb761c01cf220b29e72a8f9820776d9081
Md Aabid Hussain <181817358+mdaabid093@users.noreply.github.com>
2025-02-25 00:35:57 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker push mdaabidh14/simple-python-flask-app
+docker push mdaabidh14/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 mdaabidh14/simple-python-flask-app
+docker run -d -p 5000:5000 mdaabidh14/simple-python-flask-app:latest
